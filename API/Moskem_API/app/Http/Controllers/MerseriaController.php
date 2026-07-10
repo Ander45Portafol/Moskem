d4dd4bf709660deffdb1721b5c49c6e34b38dcc6
diegovasconcelos503 <diegoalbertovasconcelosbaiza@gmail.com>
2026-07-10 14:24:50 -0600

    crud merceria 100% API

--- API/Moskem_API/app/Http/Controllers/MerseriaController.php
+++ API/Moskem_API/app/Http/Controllers/MerseriaController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Request\MerceriaRequest;
-use App\Http\Requests\MerceriaResource;
+use App\Http\Requests\MerceriaRequest;
+use App\Http\Resources\MerceriaResource;
 use App\Http\Responses\ApiResponse;
 use App\Models\Merceria;
 use Exception;
@@ -33,6 +33,57 @@ class MerseriaController extends Controller
         }
         catch(Throwable $to){
             return ApiResponse::error('Error',500,$to->getMessage());
+        }
+    }
+    public function store(MerceriaRequest $request):JsonResponse{
+        try {
+            $validate=$request->validated();
+            $validate['visibilidad_merceria']=true;
+            $merceria=Merceria::create($validate);
+            return ApiResponse::success('Merceria creada con exito',200,new MerceriaResource($merceria));
+        } catch (\Throwable $th) {
+            return ApiResponse::error('Hay un problema con el proceso para crear',504,$th->getMessage());
+        }
+        catch(Exception $e){
+            return ApiResponse::error('Error al intentar guardar el registro',500,$e->getMessage());
+        }
+    }
+    public function show($id):JsonResponse{
+        try {
+            $merceria=Merceria::findOrFail($id);
+            return ApiResponse::success('Merceria encontrada correctamente', 200, $merceria);
+        } catch (ModelNotFoundException $me) {
+            return ApiResponse::error('Error al intentar buscar el registro',404,$me->getMessage());
+        }
+        catch(Exception $e){
+            return ApiResponse::error('No se pudo realizar la acción', 500, $e->getMessage());
+
+        }
+    }
+    public function update(MerceriaRequest $request, $id):JsonResponse{
+        try {
+            $merceria=Merceria::findOrFail($id);
+            $validate=$request->validated();
+            $merceria->update($validate);
+            return ApiResponse::success('Merceria actualizada con exito',200,new MerceriaResource($merceria));
+        } catch (ModelNotFoundException $me) {
+            return ApiResponse::error('Error al intentar buscar el registro',404,$me->getMessage());
+        }
+        catch(Exception $e){
+            return ApiResponse::error('No se pudo realizar la acción', 500, $e->getMessage());
+        }
+    }
+    public function destroy($id):JsonResponse{
+        try {
+            $merceria=Merceria::findOrFail($id);
+            $merceria->visibilidad_merceria=false;
+            $merceria->save();
+            return ApiResponse::success('Merceria eliminada con exito',200,new MerceriaResource($merceria));
+        } catch (ModelNotFoundException $me) {
+            return ApiResponse::error('Error al intentar buscar el registro',404,$me->getMessage());
+        }
+        catch(Exception $e){
+            return ApiResponse::error('No se pudo realizar la acción', 500, $e->getMessage());
         }
     }
 }
